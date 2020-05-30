69bb8f60a4dce9e3c7ca32829ec631fc3626bfdb
NoeliaIm <tuppersex.ni@gmail.com>
2020-05-30 15:11:50 +0200

    login and register layout

--- app/src/main/java/com/example/comunicat/MainActivity.java
+++ app/src/main/java/com/example/comunicat/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.comunicat;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRegister = findViewById(R.id.buttonRegistro);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegister = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intentRegister);
+            }
+        });
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLogin= new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intentLogin);
+            }
+        });
     }
+
 }
+
